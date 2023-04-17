3e4a7f9a772bddc2de00065244a7b34cffaf9924
David <davidrsmk144@gmail.com>
2023-04-16 21:40:22 -0500

    Bug in prod calls

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
   socket_url: '/',
   imgDefault: 'https://res.cloudinary.com/sennt03/image/upload/v1644190649/qxxzb23ov7corfsptvi5.png',
   peer: {
-    host: '52.207.247.119/',
+    host: '52.207.247.119',
     port: 3004,
     path: '/peerjs/connect'
   },
